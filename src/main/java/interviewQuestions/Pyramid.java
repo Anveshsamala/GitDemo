6ad07ffd9d847fb3dfb062ad157932045454ad37
Anveshsamala <samalaanvesh@gmail.com>
2026-02-10 13:45:14 +0530

    architect development

--- src/main/java/interviewQuestions/Pyramid.java
+++ src/main/java/interviewQuestions/Pyramid.java
@@ -3,12 +3,15 @@ package interviewQuestions;
 public class Pyramid {
 
 	public static void main(String[] args) {
-		for(int i=1;i<=5;i++) {
-			for(int j=1;j<=i;j++) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
 				System.out.print(j);
 			}
 			System.out.println("");
 		}
+
+		System.out.println(
+				"In new branch i am added this code in pyramid class interviewquestions package and gitdemo branch project");
 
 	}
 
